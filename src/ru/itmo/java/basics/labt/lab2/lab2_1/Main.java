573f585c3d0a9a3dd7c2cdbc0b84e86055b950fd
ZhmakinAA <ZhmakinAA@hotmail.ru>
2023-10-30 13:26:36 +0300

    Add println Java Kotlin

--- src/ru/itmo/java/basics/labt/lab2/lab2_1/Main.java
+++ src/ru/itmo/java/basics/labt/lab2/lab2_1/Main.java
@@ -2,6 +2,7 @@ package ru.itmo.java.basics.labt.lab2.lab2_1;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Java");
+        System.out.println("Java Russian");
+        System.out.println("Java Kotlin");
     }
 }
